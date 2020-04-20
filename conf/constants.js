f58124ce13151eab68c0e91892d1ad83af6c2296
Santiago <santiagocadavid@seven4n.com>
2020-04-19 20:05:19 -0500

    Fixed on type parameters on constants.js

--- conf/constants.js
+++ conf/constants.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     APP_PORT: 3000,
-    MONGO_USER: root,
-    MONGO_PASS: example,
+    MONGO_USER: 'root',
+    MONGO_PASS: 'example',
     MONGO_PORT: 27017,
-    MONGO_DB: admin
+    MONGO_DB: 'admin'
 };
